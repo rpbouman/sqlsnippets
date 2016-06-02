e3bb87c614636fc4804fae2d71f0a475fb4573ad
rpbouman <roland.bouman@gmail.com>
2016-06-02 23:02:16 +0200

    License header, documentation, newlines in generated SQL

--- nullable_columns_that_are_not_null.hana.sql
+++ nullable_columns_that_are_not_null.hana.sql
@@ -1,7 +1,39 @@
-select  'select cast(null as nvarchar(128)) as "COLUMN_NAME" from dummy where 1=0 '
+/**
+* Copyright 2016 Roland.Bouman@gmail.com, Roland.Bouman@just-bi.nl,
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* Name: nullable_columns_tat_are_not_null
+* ---------------------------------------
+*
+* Parameters:
+* -----------
+* 1) SCHEMA_NAME (optional, default: CURRENT_SCHEMA)
+* 2) TABLE_NAME
+*
+* Description:
+* ------------
+* Uses the parameters to generate a SQL query that lists all nullable columns 
+* which in fact do not contain any null values. 
+* In other words, columns could be altered to be NOT NULL without error.
+*
+**/
+
+select  'select cast(null as nvarchar(128)) as "COLUMN_NAME" '
+||      'from dummy '
+||      'where 1=0 '
 ||      STRING_AGG(stmt) as stmt
 from    (
-        select ' union all '
+        select char(10)||'union all'||char(10)
         ||     'select '''||column_name||''' '
         ||     'from "'||schema_name||'"."'||table_name||'" '
         ||     'having count(*) = count('||column_name||')' as stmt
@@ -11,3 +43,4 @@ from    (
         and    is_nullable  = 'TRUE'
         order by position
         )
+;
